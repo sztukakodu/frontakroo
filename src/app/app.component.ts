113c09ec02ad1c9cf49b615ba22ab2dc4b432f69
Dariusz Mydlarz <dmydlarz@gmail.com>
2021-04-19 13:57:24 +0200

    11.3 - Catalog Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CatalogService} from "./catalog.service";
+import {Book} from "./book";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontakroo';
+
+  constructor(private catalogService: CatalogService) {}
+
+  public getCatalog(): void {
+    this.catalogService.getCatalog().subscribe(
+      (response: Book[]) => {
+        console.log("Got book list:");
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error.message);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.getCatalog();
+  }
 }
