db2e82fbd3edcfc4a1b17b8ae5d3aec71d585e8f
Dariusz Mydlarz <dmydlarz@gmail.com>
2021-04-20 08:25:46 +0200

    11.9 Koszyk budowania UI-a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent implements OnInit {
   public catalog: Book[];
   public cartItems: CartItem[] = [];
 
-  constructor(private catalogService: CatalogService) {}
+  constructor(private catalogService: CatalogService) {
+  }
 
   public getCatalog(): void {
     this.catalogService.getCatalog().subscribe(
@@ -30,11 +31,11 @@ export class AppComponent implements OnInit {
   }
 
   public addToCart(book: Book): void {
-    const current: CartItem = this.cartItems.find(x => x.bookId === book.id);
+    const current: CartItem = this.cartItems.find(x => x.book.id === book.id);
     if (current) {
       current.quantity += 1;
     } else {
-      this.cartItems.push({bookId: book.id, quantity: 1} as CartItem);
+      this.cartItems.push({book: book, quantity: 1} as CartItem);
     }
     console.log('Cart is: ' + JSON.stringify(this.cartItems));
   }
@@ -44,7 +45,32 @@ export class AppComponent implements OnInit {
       .reduce((sum, current) => sum + current.quantity, 0);
   }
 
+  public totalCartAmount(): number {
+    return this.cartItems
+      .reduce((sum, current) => sum + current.book.price * current.quantity, 0);
+  }
+
+  public clearCart(): void {
+    this.cartItems = []
+  }
+
   ngOnInit(): void {
     this.getCatalog();
+  }
+
+  public onOpenModal(mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'cart') {
+      button.setAttribute('data-target', '#cartModal');
+    }
+    // if (mode === 'order') {
+    //   button.setAttribute('data-target', '#orderModal');
+    // }
+    container.appendChild(button);
+    button.click();
   }
 }
